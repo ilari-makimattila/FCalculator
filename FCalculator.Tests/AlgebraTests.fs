c9c0ba999fe17ba6e418ce3bbb482c0b50aef7c0
Ilari Mäkimattila <ilari.makimattila@gmail.com>
2013-02-11 18:21:29 +0200

    Refactored to a library and added simple unit tests

---
+++ FCalculator.Tests/AlgebraTests.fs
@@ -0,0 +1,28 @@
+module FCalculator.Tests.Algebra
+
+open NUnit.Framework
+open FsUnit
+open FCalculator.Main
+
+[<TestFixture>]
+type ``Given EvaluateString is given a valid expression`` ()=
+    
+    [<Test>] member this.
+        ``when expression is 1 + 2 answer is 3.0`` ()=
+            EvaluateString "1 + 2" |> should equal 3.0
+
+    [<Test>] member this.
+        ``when expression is 1 - 2 answer is -1.0`` ()=
+            EvaluateString "1 - 2" |> should equal -1.0
+            
+     [<Test>] member this.
+        ``when expression is 2 * 2 answer is 4.0`` ()=
+            EvaluateString "2 * 2" |> should equal 4.0
+            
+     [<Test>] member this.
+        ``when expression is 3 / 2 answer is 1.5`` ()=
+            EvaluateString "3 / 2" |> should equal 1.5
+            
+     [<Test>] member this.
+        ``when expression is 2 * 2 / 1 + 2 * 3 - 6 answer is 4.0`` ()=
+            EvaluateString "2 * 2 / 1 + 2 * 3 - 6" |> should equal 4.0
